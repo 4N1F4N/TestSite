aa258c62f3842e18e64c953ba517385f1e832c85
4N1F4N <6t3f4n@gmail.com>
2021-11-24 18:09:58 +0300

    errorsoutput

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,17 @@
 class SessionsController < ApplicationController
     def create
-        user = User.find_by(login_params)
+        user = User.find_by(login:login_params[:login])
         if user
-            session[:user_id] = user.id
-            redirect_to user
+            if user[:password] == login_params[:password]
+                session[:user_id] = user.id 
+                redirect_to user
+            else
+                flash[:login_errors] = ["Неправильный пароль"]
+            end
         else
-            #flash[:login_errors] = ['invaild credentials']
-            redirect_to '/'
+            flash[:login_errors] = ["Неправильный логин"]
         end
+        redirect_to '/'
     end
     def show
         @user = User.find(params[:id])
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,21 +9,25 @@ class UsersController < ApplicationController
         
     end
     def create
-        if !User.find_by(login:user_params[:login]) && !(user_params[:login] == "") && !(user_params[:password] == "") 
-            user = User.new(user_params)
-            if user.save 
-                session[:user_id] = user.id 
-                redirect_to access_path
+        if !User.find_by(login:user_params[:login])
+            if !(user_params[:login] == "")
+                if !(user_params[:password] == "")
+                    user = User.new(user_params)
+                    session[:user_id] = user.id 
+                    redirect_to access_path
+                else
+                    flash[:register_errors] = ["Пустой пароль"]
+                end
             else
-                #flash[:register_errors] = user.errors.full_messages
-                redirect_to '/'
+                flash[:register_errors] = ["Пустой логин"]
             end
         else
-            #flash[:register_errors] = user.errors.full_messages
-            redirect_to '/'
+            flash[:register_errors] = ["Такой логин уже есть"]
         end
+        redirect_to '/'
     end
-    private def user_params
-        params.require(:user).permit(:login, :password)
-    end
+    private 
+        def user_params
+            params.require(:user).permit(:login, :password)
+        end
 end
